48e9d8e3c0093e9e2424e608d8f13731a30435a4
RickardTimergalejevs <rickard.time@gmail.com>
2023-12-27 14:30:54 +0100

    Frontend installation (structure, scss, packages, prettier/eslint, vite)

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>App</h1>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
