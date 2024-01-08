8161ffc89f13f5c347e51994e24903f017338a08
RickardTimergalejevs <rickard.time@gmail.com>
2024-01-08 15:10:48 +0100

    build: bcrypt package

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model, models } from 'mongoose'
+import bcrypt from 'bcrypt'
 
 export interface IUser {
   firstName: string
@@ -6,6 +7,10 @@ export interface IUser {
   email: string
   password: string
   location: string
+  comparePassword: (
+    password: string,
+    callback: (error: Error | null, isMatch?: boolean) => void,
+  ) => void
 }
 
 const userSchema = new Schema<IUser>({
@@ -15,6 +20,43 @@ const userSchema = new Schema<IUser>({
   password: { type: String, required: true },
   location: { type: String, required: true },
 })
+
+userSchema.pre('save', function (next) {
+  const user = this
+
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, function (saltError, salt) {
+      if (saltError) {
+        return next(saltError)
+      } else {
+        bcrypt.hash(user.password, salt, function (hashError, hash) {
+          if (hashError) {
+            return next(hashError)
+          }
+
+          user.password = hash
+          next()
+        })
+      }
+    })
+  } else {
+    return next()
+  }
+})
+
+userSchema.methods.comparePassword = function (
+  this: IUser,
+  password: string,
+  callback: (error: Error | null, isMatch?: boolean) => void,
+) {
+  bcrypt.compare(password, this.password, function (error, isMatch) {
+    if (error) {
+      return callback(error)
+    } else {
+      callback(null, isMatch)
+    }
+  })
+}
 
 const UserModel = models.user || model<IUser>('User', userSchema)
 
