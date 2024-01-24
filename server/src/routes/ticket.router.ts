d0257a5658326a77fbed4b02fca770ac08f5a9fe
RickardTimergalejevs <rickard.time@gmail.com>
2024-01-24 14:37:30 +0100

    feat: create ticket controller

--- server/src/routes/ticket.router.ts
+++ server/src/routes/ticket.router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
+import { createTicket } from '../controllers/ticket.controller'
 
-const ticketRouter = Router()
+const ticketRouter = Router().post('/tickets', createTicket)
 
 export default ticketRouter
