5423198df9c9163289364af33b382dc19d5d7c17
RickardTimergalejevs <rickard.time@gmail.com>
2024-01-29 16:20:09 +0100

    feat: create session structure on admin page

--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,5 +1,38 @@
+import Button from '../../components/common/Button/Button'
+import Input from '../../components/common/Input/Input'
+
 const Admin = () => {
-  return <div>Admin</div>
+  return (
+    <div>
+      <h1>Admin</h1>
+      <h1>Create new session</h1>
+      <div>
+        <p>Select movie</p>
+        <div>
+          <select name="" id="">
+            <option value="option1">Variant 1</option>
+          </select>
+        </div>
+        <div>
+          <p>Select city</p>
+          <div>
+            <select name="" id="">
+              <option value="option1">Variant 1</option>
+            </select>
+          </div>
+        </div>
+        <div>
+          <input type="date" />
+          <p>ShowDate</p>
+        </div>
+        <div>
+          <input type="time" />
+          <p>DisplayType</p>
+        </div>
+      </div>
+      <Button children="Create" color="green" />
+    </div>
+  )
 }
 
 export default Admin
