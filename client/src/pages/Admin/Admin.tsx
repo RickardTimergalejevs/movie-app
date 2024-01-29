37f92bc5b25ef2d35a2beaa3206b18723bd534cb
RickardTimergalejevs <rickard.time@gmail.com>
2024-01-29 21:04:33 +0100

    feat: success message on create

--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,14 +1,21 @@
-import { Field, FieldProps, Form, Formik } from 'formik'
-import Button from '../../components/common/Button/Button'
+import {
+  ErrorMessage,
+  Field,
+  FieldProps,
+  Form,
+  Formik,
+  FormikHelpers,
+} from 'formik'
 import Input from '../../components/common/Input/Input'
 import * as Yup from 'yup'
+import './Admin.scss'
 
 import { useGetPlayingMoviesQuery } from '../../redux/services/movies'
 import { useCreateSessionMutation } from '../../redux/services/sessions'
 
 const Admin = () => {
   const { data: movie, isLoading, isError } = useGetPlayingMoviesQuery(1)
-  const [createSession] = useCreateSessionMutation()
+  const [createSession, { isSuccess }] = useCreateSessionMutation()
 
   interface ISessionValues {
     movieId: number
@@ -26,8 +33,6 @@ const Admin = () => {
     displayType: '',
   }
 
-  console.log(initialValuesSession)
-
   const loginSchema = Yup.object().shape({
     movieId: Yup.number().required('Movie is required'),
     city: Yup.string().required('City is required'),
@@ -36,47 +41,86 @@ const Admin = () => {
     displayType: Yup.string().required('Display type is required'),
   })
 
-  const handleSubmit = async (values: ISessionValues) => {
+  const handleSubmit = async (
+    values: ISessionValues,
+    { resetForm }: FormikHelpers<ISessionValues>,
+  ) => {
     await createSession(values)
+    resetForm()
   }
 
   return (
-    <div>
-      <h1>Admin</h1>
-      <h1>Create new session</h1>
-      <Formik
-        key="session"
-        initialValues={initialValuesSession}
-        validationSchema={loginSchema}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <label>Select movie</label>
-          <Field as="select" name="movieId" id="movieId">
-            <option value="">Select a movie</option>
-            {movie?.results?.map((movie) => (
-              <option key={movie.id} value={movie.id}>
-                {movie.title}
-              </option>
-            ))}
-          </Field>
-          <Field as="select" name="city" id="city">
-            <option value="">Select a city</option>
-            <option value="Stockholm">Stockholm</option>
-          </Field>
-          <label>Select date</label>
-          <Field type="date" name="showDate" id="showDate" />
-          <label>Select time</label>
-          <Field type="time" name="showTime" id="showTime" />
-          <Field as="select" name="displayType">
-            <option value="">Select a display type</option>
-            <option value="2D">2D</option>
-            <option value="3D">3D</option>
-            <option value="IMAX">IMAX</option>
-          </Field>
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+    <div className="admin__page">
+      <div className="admin-nav">
+        <hr />
+        <h1 className="admin-nav__title">Admin</h1>
+      </div>
+      <div className="admin-wrapper">
+        <h1>Create new session</h1>
+        <Formik
+          key="session"
+          initialValues={initialValuesSession}
+          validationSchema={loginSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form className="session-form">
+            <label>Select movie</label>
+            <Field as="select" name="movieId" id="movieId">
+              <option value="">---</option>
+              {movie?.results?.map((movie) => (
+                <option key={movie.id} value={movie.id}>
+                  {movie.title}
+                </option>
+              ))}
+            </Field>
+            <ErrorMessage
+              name="movieId"
+              component="div"
+              className="error-message"
+            />
+            <label>Select city</label>
+            <Field as="select" name="city" id="city">
+              <option value="">---</option>
+              <option value="Stockholm">Stockholm</option>
+            </Field>
+            <ErrorMessage
+              name="city"
+              component="div"
+              className="error-message"
+            />
+            <label>Select date</label>
+            <Field
+              type="date"
+              name="showDate"
+              id="showDate"
+              component={Input}
+            />
+            <label>Select time</label>
+            <Field
+              type="time"
+              name="showTime"
+              id="showTime"
+              component={Input}
+            />
+            <label>Select display type</label>
+            <Field as="select" name="displayType">
+              <option value="">---</option>
+              <option value="2D">2D</option>
+              <option value="3D">3D</option>
+              <option value="IMAX">IMAX</option>
+            </Field>
+            <ErrorMessage
+              name="displayType"
+              component="div"
+              className="error-message"
+            />
+            <button className="session-form__btn" type="submit">
+              Submit
+            </button>
+          </Form>
+        </Formik>
+        {isSuccess && <div>Session created!</div>}
+      </div>
     </div>
   )
 }
